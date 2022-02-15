ad947cd07eb10c31ba6b78ad1486f99a35b3a10d
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2022-02-15 15:34:56 +0100

    inizio

---
+++ snack-d/index.php
@@ -0,0 +1,21 @@
+<!-- Snack D
+Prendere un paragrafo abbastanza lungo, contenente diverse frasi. 
+Prendere il paragrafo e suddividerlo in tanti paragrafi. 
+Ogni punto un nuovo paragrafo. -->
+
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack D</title>
+</head>
+<body>
+    
+</body>
+</html>
