0f7407ddaba324cb858be6315360db82d3c2f8c1
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2022-02-16 00:55:26 +0100

    snack b

--- snack-b/index.php
+++ snack-b/index.php
@@ -6,6 +6,20 @@ Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” 
 
 <?php
 
+    $name = $_GET['name'];
+    $mail = $_GET['mail'];
+    $age = $_GET['age'];
+
+    if(
+        strlen($name) > 3 && 
+        strpos($mail, '@') !== false && 
+        strpos($mail, '.') !== false && 
+        is_numeric($age) == true){
+        echo 'valida';
+    }else{
+        echo 'riprova';
+    };
+
 ?>
 
 <!DOCTYPE html>
