dc6c0e2abf295051a9df37e796f53b93edbd167a
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2022-02-15 16:10:03 +0100

    squadre e punteggio

--- snack-a/index.php
+++ snack-a/index.php
@@ -4,9 +4,35 @@ Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squ
 Stampiamo a schermo tutte le partite con questo schema.
 Olimpia Milano - Cantù | 55-60  -->
 
+</pre>
 <?php
+    $partiteArray = [
+        [
+            'squadra_1' => 'Miami Heat',
+            'squadra_2' => 'New York Knicks',
+            'punti_squadra_1' => 30,
+            'punti_squadra_2' => 25,
+        ],
+        [
+            'squadra_1' => 'Orlando Magic',
+            'squadra_2' => 'Chicago Bulls',
+            'punti_squadra_1' => 70,
+            'punti_squadra_2' => 20,
+        ],
+        [
+            'squadra_1' => 'Boston Celtics',
+            'squadra_2' => 'Utah Jazz',
+            'punti_squadra_1' => 20,
+            'punti_squadra_2' => 15,
+        ], 
+    ];
+
+        
+    echo var_dump($partiteArray);
+        
 
 ?>
+</pre>
 
 <!DOCTYPE html>
 <html lang="en">
